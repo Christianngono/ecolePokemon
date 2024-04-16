6ee57ea90a09100d99656e1c3dd1ca08b771f261
Christianngono <christianngonoabanda384@gmail.com>
2024-04-16 17:51:54 +0200

    Ajout nouveau

--- functions.go
+++ functions.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func sortPokemons(pokemonList []string) []string {
+func SortPokemons(pokemonList []string) []string {
 	var sortedPokemonList []string
 
 	for _, pokemon := range pokemonList {
@@ -20,44 +20,44 @@ func sortPokemons(pokemonList []string) []string {
 					break
 				}
 			}
-			if isLast { 
-                sortedPokemonList = append(sortedPokemonList, pokemon)
-            }
+			if isLast {
+				sortedPokemonList = append(sortedPokemonList, pokemon)
+			}
 		}
-	} 
+	}
 	return sortedPokemonList
 }
 
-func sortCreationDate(pokemonList []string) []string {
+func SortVersions(pokemonForm []string) []string {
 	var sortedPokemonList []string
 
-    for _, pokemon := range pokemonList {
-        var isLast = true
-        if len(sortedPokemonList) == 0 {
-            sortedPokemonList = append(sortedPokemonList, pokemon)
-        } else {
-            for i, sortedPokemon := range sortedPokemonList {
-                if pokemon <= sortedPokemon {
-                    sortedPokemonList = slices.Insert(sortedPokemonList, i, pokemon)
-                    isLast = false
-                    break
-                }
-            }
-            if isLast { 
-                sortedPokemonList = append(sortedPokemonList, pokemon)
-            }
-        }
-    } 
-    return sortedPokemonList
+	for _, pokemon := range pokemonForm {
+		var isLast = true
+		if len(sortedPokemonList) == 0 {
+			sortedPokemonList = append(sortedPokemonList, pokemon)
+		} else {
+			for i, sortedPokemon := range sortedPokemonList {
+				if pokemon <= sortedPokemon {
+					sortedPokemonList = slices.Insert(sortedPokemonList, i, pokemon)
+					isLast = false
+					break
+				}
+			}
+			if isLast {
+				sortedPokemonList = append(sortedPokemonList, pokemon)
+			}
+		}
+	}
+	return sortedPokemonList
 }
 
 func SearchPokemons(input string, pokemonList []string) []string {
 	var searchedPokemonList []string
 
-    for _, pokemon := range pokemonList {
-        if strings.Contains(strings.ToLower(pokemon), strings.ToLower(strings.TrimSpace(input))) {
-            searchedPokemonList = append(searchedPokemonList, pokemon)
-        }
-    }
-    return searchedPokemonList
+	for _, pokemon := range pokemonList {
+		if strings.Contains(strings.ToLower(pokemon), strings.ToLower(strings.TrimSpace(input))) {
+			searchedPokemonList = append(searchedPokemonList, pokemon)
+		}
+	}
+	return searchedPokemonList
 }
