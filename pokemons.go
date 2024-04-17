f0422ca7a9c0d538de06036421fae151302a6033
Christianngono <christianngonoabanda384@gmail.com>
2024-04-17 08:05:07 +0200

    Ajout de pokemons.go

--- pokemons.go
+++ pokemons.go
@@ -8,8 +8,11 @@ import (
 	"strconv"
 )
 
-func GetAllPokemons() []Pokemon {
-	var pokemonList []Pokemon
+const url = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+func GetAllPokemons() []string {
+	var pokemonList []string
+
 
 	response, err := http.Get(url + "/pokemons") // calls the /pokemon section of the API
 	if err != nil {
@@ -18,24 +21,24 @@ func GetAllPokemons() []Pokemon {
 	}
 
 	defer response.Body.Close()
-	body, err2 := io.ReadAll(response.Body) // Read the response
-	if err2 != nil {
-		fmt.Println("Reading Error in GetAllPokemons :", err2)
+	body, err := io.ReadAll(response.Body) // Read the response
+	if err != nil {
+		fmt.Println("Reading Error in GetAllPokemons :", err)
 		return nil
 	}
 
-	err3 := json.Unmarshall(body, &pokemonList)
+	err3 := json.Unmarshal(body, &pokemonList)
 	if err3 != nil {
-		fmt.Println("Error Unmarshall in GetAllPokemons :", err3)
+		fmt.Println("Error Unmarshal in GetAllPokemons :", err3)
 		return nil
 	}
 
 	return pokemonList
 }
 
-func GetPokemon(id int) Pokemon {
+func GetPokemon(id int) string {
 
-	var pokemon Pokemon
+	var pokemon string
 
 	response, err := http.Get(url + "/pokemons/" + strconv.Itoa(id))
 	if err != nil {
@@ -50,24 +53,24 @@ func GetPokemon(id int) Pokemon {
 		return pokemon
 	}
 
-	err3 := json.Unmarshall(body, &pokemon)
+	err3 := json.Unmarshal(body, &pokemon)
 	if err3 != nil {
-		fmt.Println("Error Unmarshall in GetPokemon:", err3)
+		fmt.Println("Error Unmarshal in GetPokemon:", err3)
 		return pokemon
 	}
 	return pokemon
 }
 
-func NametoPokemon(name string) Pokemon {
+func NametoPokemon(name string) string {
 	pokemonList := GetAllPokemons()
 
 	for _, pokemon := range pokemonList {
-		if pokemon.Name == name {
+		if pokemon == name {
 			return pokemon
 		}
 	}
 
-	var error pokemon // If no pokemon fonud
+	var error string // If no pokemon fonud
 	fmt.Println("Error, name not found")
 	return error
 }
