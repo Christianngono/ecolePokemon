ac8fd089e5a0693011bd3437aab40231301f994c
Christianngono <christianngonoabanda384@gmail.com>
2024-05-11 01:47:43 +0200

    To renew pokemons.go

--- pokemons.go
+++ pokemons.go
@@ -15,16 +15,20 @@ type PokemonResponse struct {
 	ApiTypes []ApiType `json:"apiTypes"`
 	Stats    Stats `json:"stats"`
 	ResistanceModifyingAbilitiesForApis []ResistanceModifyingAbilitiesForApi `json:"resistanceModifyingAbilitiesForApis"`
-	ApiGenerations []ApiGeneration `json:"apiGenerations"`
-	ApiResistances []ApiResistance `json:"apiResistances"`	
+	ApiGenerations  []int `json:"apiGenerations"`
+	ApiResistances []ApiResistance `json:"apiResistances"`
+    ApiEvolutions []ApiEvolution `json:"apiEvolutions"`
+    ApiPreEvolutions []ApiPreEvolution `json:"apiPreEvolutions"`
+    ApiResistancesWithAbilities []ApiResistancesWithAbility `json:"apiResistancesWithAbilities"`
+
 }
 
 type Stats struct {
 	HP int `json:"hp"`
 	Attack int `json:"attack"`
 	Defense int `json:"defense"`
-	SpecialAttack int `json:"specialAttack"`
-	SpecialDefense int `json:"specialDefense"`
+	SpecialAttack int `json:"special_attack"`
+	SpecialDefense int `json:"special_defense"`
 	Speed int `json:"speed"`
 }
 
@@ -37,18 +41,34 @@ type ResistanceModifyingAbilitiesForApi struct {
     Name string `json:"name"`
     Image string `json:"image"`
 }
+
 type ApiGeneration struct {
-    Name string `json:"name"`
-    Image string `json:"image"`
+    ApiGeneration int `json:"apiGeneration" ` 
 }
 
 
 type ApiResistance struct {
 	Name string `json:"name"`
-	Image string `json:"image"`
 	DamageMultiplier float64 `json:"damage_multipliers"`
 	DamageRelation string `json:"damage_relation"`
 }
+
+type ApiEvolution struct {
+    Name string `json:"name"`
+    PokedexId int `json:"pokedexId"`
+    Image string `json:"image"`
+}
+
+type ApiPreEvolution struct {
+    Name string `json:"name"`
+    PokedexId int `json:"pokedexId"`
+}
+
+type ApiResistancesWithAbility struct {
+    Name string `json:"name"`
+    Image string `json:"image"`
+}
+
 
 func GetAllPokemons() ([]PokemonResponse, error) {
 	var pokemons []PokemonResponse
@@ -161,25 +181,7 @@ func GetPokemonResistanceModifyingAbilitiesForApisByName(name string) (PokemonRe
     }
     return pokemon, nil
 }
-
-func GetAllPokemonsApiGenerations()([]PokemonResponse, error) {
-	var pokemons []PokemonResponse
-    resp, err := http.Get(url)
-    if err!= nil {
-        return pokemons, err
-    }
-    defer resp.Body.Close()
-    if resp.StatusCode!= http.StatusOK {
-        return pokemons, fmt.Errorf("erreur lors de la récupération de la liste des Pokémons")
-    }
-
-    if err := json.NewDecoder(resp.Body).Decode(&pokemons); err != nil {
-        return pokemons, err
-    }
-    return pokemons, nil
-}
-
-func GetPokemonApiGenerationsByName(name string) (PokemonResponse, error) {
+func GetPokemonApiGeneration(name string) (PokemonResponse, error) {
 	var pokemon PokemonResponse
 
     response, err := http.Get(url + name)
@@ -213,6 +215,102 @@ func GetAllPokemonsApiResistances()([]PokemonResponse, error) {
 
 func GetPokemonApiResistancesByName(name string) (PokemonResponse, error) {
 	var pokemon PokemonResponse
+
+    response, err := http.Get(url + name)
+    if err != nil {
+        return pokemon, err
+    }
+    defer response.Body.Close()
+
+    if err := json.NewDecoder(response.Body).Decode(&pokemon); err != nil {
+        return pokemon, err
+    }
+    return pokemon, nil
+}
+
+func GetAllPokemonsApiEvolutions()([]PokemonResponse, error) {
+    var pokemons []PokemonResponse
+    resp, err := http.Get(url)
+    if err!= nil {
+        return pokemons, err
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode!= http.StatusOK {
+        return pokemons, fmt.Errorf("erreur lors de la récupération de la liste des Pokémons")
+    }
+
+    if err := json.NewDecoder(resp.Body).Decode(&pokemons); err != nil {
+        return pokemons, err
+    }
+    return pokemons, nil
+}
+
+func GetPokemonApiEvolutionsByName(name string) (PokemonResponse, error) {
+    var pokemon PokemonResponse
+
+    response, err := http.Get(url + name)
+    if err != nil {
+        return pokemon, err
+    }
+    defer response.Body.Close()
+
+    if err := json.NewDecoder(response.Body).Decode(&pokemon); err != nil {
+        return pokemon, err
+    }
+    return pokemon, nil
+}
+
+func GetAllPokemonsApiPreEvolutions()([]PokemonResponse, error) {
+    var pokemons []PokemonResponse
+    resp, err := http.Get(url)
+    if err!= nil {
+        return pokemons, err
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode!= http.StatusOK {
+        return pokemons, fmt.Errorf("erreur lors de la récupération de la liste des Pokémons")
+    }
+
+    if err := json.NewDecoder(resp.Body).Decode(&pokemons); err != nil {
+        return pokemons, err
+    }
+    return pokemons, nil
+}
+
+func GetPokemonApiPreEvolutionsByName(name string) (PokemonResponse, error) {
+    var pokemon PokemonResponse
+
+    response, err := http.Get(url + name)
+    if err != nil {
+        return pokemon, err
+    }
+    defer response.Body.Close()
+
+    if err := json.NewDecoder(response.Body).Decode(&pokemon); err != nil {
+        return pokemon, err
+    }
+    return pokemon, nil
+}
+
+func GetAllPokemonsApiResistancesWithAbilities()([]PokemonResponse, error) {
+    var pokemons []PokemonResponse
+    resp, err := http.Get(url)
+    if err!= nil {
+        return pokemons, err
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode!= http.StatusOK {
+        return pokemons, fmt.Errorf("erreur lors de la récupération de la liste des Pokémons")
+    }
+
+    if err := json.NewDecoder(resp.Body).Decode(&pokemons); err != nil {
+        return pokemons, err
+    }
+    return pokemons, nil
+}
+
+func GetPokemonApiResistancesWithAbilitiesByName(name string) (PokemonResponse, error) {
+    var pokemon PokemonResponse
 
     response, err := http.Get(url + name)
     if err != nil {
